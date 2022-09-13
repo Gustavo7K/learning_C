09e88809f6fec4860869e02535c966a67d37f44f
Gustavo Henrique <85465135+Gustavo7K@users.noreply.github.com>
2022-09-13 04:33:04 -0300

    Update ex11.cpp

--- ex11.cpp
+++ ex11.cpp
@@ -1,4 +1,4 @@
-//11. Faça um programa que leia dois valores reais e exiba o primeiro com acréscimo de 30%, e o segundo com desconto de 25%.
+//11. FaÃ§a um programa que leia dois valores reais e exiba o primeiro com acrÃ©scimo de 30%, e o segundo com desconto de 25%.
 
 # include <stdio.h>
 # include <conio.h>
@@ -8,17 +8,14 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float n1, n2, aum, desc;
-	printf("Digite um número ");
+	printf("Digite um nÃºmero ");
 	scanf("%f",&n1);
-	printf("Digite um número ");
+	printf("Digite um nÃºmero ");
 	scanf("%f",&n2);
 	aum= n1*1.30;
 	desc= n2-n2*0.25;
-	printf("O numero %.2f com aumento de 30%% é %.2f",n1,aum);
-	printf("\nO numero %.2f com desconto de 25%% é %.2f",n2,desc);
+	printf("O numero %.2f com aumento de 30%% Ã© %.2f",n1,aum);
+	printf("\nO numero %.2f com desconto de 25%% Ã© %.2f",n2,desc);
 	getch();
 	return 0;
 }
-// terminei essa bosta sozinho vai se fuder todo mundo
-//q se foda
-//pouco pa nois é muito
